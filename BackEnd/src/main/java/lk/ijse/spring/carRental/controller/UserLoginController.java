c01c4b8a8ed5b407e45821f77e8500a896ffb438
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-20 17:16:13 +0530

    added controllers

---
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/controller/UserLoginController.java
@@ -0,0 +1,17 @@
+package lk.ijse.spring.carRental.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * `@authority Tharindu Dilan`
+ * 4:58 PM
+ * 2023-10-20 - 10 - 2023
+ */
+
+@RestController
+@RequestMapping("/login")
+@CrossOrigin
+public class UserLoginController {
+}
