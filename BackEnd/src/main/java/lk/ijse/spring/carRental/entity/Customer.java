4cf6ae12e545e40e50459fb5022650a781768ebd
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-20 12:27:14 +0530

    Create Customer.java

---
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/entity/Customer.java
@@ -0,0 +1,37 @@
+package lk.ijse.spring.carRental.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+/**
+ * `@authority Tharindu Dilan`
+ * 12:08 PM
+ * 2023-10-20 - 10 - 2023
+ */
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class Customer {
+    @Id
+    @Column(length = 10)
+    private String cid;
+    private String name;
+    private String email;
+    private String address;
+    private String contact;
+    private LocalDate regDte;
+    private String idNo;
+    @Lob
+    private byte[] imageData;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private User user;
+}
