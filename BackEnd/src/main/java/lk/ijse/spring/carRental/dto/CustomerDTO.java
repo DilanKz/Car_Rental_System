2cf188e4aa42d1bc6783209fbfbfceacd2d5f82e
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-22 15:39:50 +0530

    Create CustomerDTO.java

---
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/dto/CustomerDTO.java
@@ -0,0 +1,32 @@
+package lk.ijse.spring.carRental.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.persistence.Lob;
+import java.time.LocalDate;
+
+/**
+ * `@authority Tharindu Dilan`
+ * 2:56 PM
+ * 2023-10-22 - 10 - 2023
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class CustomerDTO {
+    private String cid;
+    private String name;
+    private String email;
+    private String address;
+    private String contact;
+    private LocalDate regDte;
+    private String idNo;
+    private byte[] imageFront;
+    private byte[] imageBack;
+    private UserDTO dto;
+}
