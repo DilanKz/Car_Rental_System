edce1e111d09a7878dd3eccfbaeae1a89fbf3582
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-22 20:39:22 +0530

    Create UserRegisterController.java

---
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/controller/UserRegisterController.java
@@ -0,0 +1,47 @@
+package lk.ijse.spring.carRental.controller;
+
+import lk.ijse.spring.carRental.dto.CustomerDTO;
+import lk.ijse.spring.carRental.dto.responseDTOs.CustomerRespDTO;
+import lk.ijse.spring.carRental.service.CustomerService;
+import lk.ijse.spring.carRental.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+/**
+ * `@authority Tharindu Dilan`
+ * 8:17 PM
+ * 2023-10-22 - 10 - 2023
+ */
+@RestController
+@RequestMapping("/Register")
+@CrossOrigin
+public class UserRegisterController {
+
+    @Autowired
+    CustomerService service;
+
+    @PostMapping
+    public ResponseUtil makeAccount(CustomerRespDTO dto) throws IOException {
+
+        CustomerDTO customerDTO = new CustomerDTO(
+                dto.getCid(),
+                dto.getName(),
+                dto.getEmail(),
+                dto.getAddress(),
+                dto.getContact(),
+                dto.getRegDte(),
+                dto.getIdNo(),
+                dto.getImageFront().getBytes(),
+                dto.getImageBack().getBytes(),
+                dto.getDto()
+        );
+
+        service.saveCustomer(customerDTO);
+        return new ResponseUtil("Ok","Successfully create account",dto);
+    }
+}
