e772582599f24f0a6a5725d3d8074b5b2cca8de6
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-22 15:39:44 +0530

    Update UserDTO.java

--- BackEnd/src/main/java/lk/ijse/spring/carRental/dto/UserDTO.java
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/dto/UserDTO.java
@@ -1,13 +1,20 @@
 package lk.ijse.spring.carRental.dto;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 
 /**
  * `@authority Tharindu Dilan`
  * 2:43 PM
  * 2023-10-22 - 10 - 2023
  */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class UserDTO {
 
     private String uid;
