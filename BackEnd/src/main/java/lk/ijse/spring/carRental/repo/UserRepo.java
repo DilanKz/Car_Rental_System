fda83230107090960dd89dff0acc6521e3b83fe0
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-22 14:55:52 +0530

    Update UserRepo.java

--- BackEnd/src/main/java/lk/ijse/spring/carRental/repo/UserRepo.java
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/repo/UserRepo.java
@@ -2,6 +2,7 @@ package lk.ijse.spring.carRental.repo;
 
 import lk.ijse.spring.carRental.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * `@authority Tharindu Dilan`
@@ -9,4 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * 2023-10-20 - 10 - 2023
  */
 public interface UserRepo extends JpaRepository<User,String> {
+    @Query(value = "SELECT id FROM User ORDER BY id DESC LIMIT 1",nativeQuery = true)
+    String getLastID();
+
 }
