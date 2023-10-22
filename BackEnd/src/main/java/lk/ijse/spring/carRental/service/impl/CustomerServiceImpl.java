4f895d700e59c604476fd475356b378a16c57405
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-22 15:40:14 +0530

    Create CustomerServiceImpl.java

---
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,43 @@
+package lk.ijse.spring.carRental.service.impl;
+
+import lk.ijse.spring.carRental.dto.CustomerDTO;
+import lk.ijse.spring.carRental.entity.Customer;
+import lk.ijse.spring.carRental.repo.CustomerRepo;
+import lk.ijse.spring.carRental.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * `@authority Tharindu Dilan`
+ * 3:03 PM
+ * 2023-10-22 - 10 - 2023
+ */
+public class CustomerServiceImpl implements CustomerService {
+    @Autowired
+    CustomerRepo customerRepo;
+
+    @Autowired
+    ModelMapper mapper;
+
+    @Override
+    public void saveCustomer(CustomerDTO dto){
+
+        if (!customerRepo.existsById(dto.getCid())){
+            throw new RuntimeException(dto.getCid()+" Customer already exist");
+        }
+
+        Customer customer = mapper.map(dto, Customer.class);
+        customerRepo.save(customer);
+    }
+
+    @Override
+    public void updateCustomer(CustomerDTO c) {
+        if (!customerRepo.existsById(c.getCid())) {
+            throw new RuntimeException(c.getCid()+ " Customer is not available, check again.!");
+        }
+
+        Customer map = mapper.map(c, Customer.class);
+        customerRepo.save(map);
+    }
+
+}
