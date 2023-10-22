369931b03f68466f774dad04eac93f49b3bb0ccc
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-22 15:40:02 +0530

    Create CustomerService.java

---
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/service/CustomerService.java
@@ -0,0 +1,14 @@
+package lk.ijse.spring.carRental.service;
+
+import lk.ijse.spring.carRental.dto.CustomerDTO;
+
+/**
+ * `@authority Tharindu Dilan`
+ * 3:02 PM
+ * 2023-10-22 - 10 - 2023
+ */
+public interface CustomerService {
+    void saveCustomer(CustomerDTO dto);
+
+    void updateCustomer(CustomerDTO c);
+}
