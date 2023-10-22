e7055a61f7754448829d9aa6c173ef1efa4ed457
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-22 14:55:48 +0530

    Create UserDTO.java

---
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package lk.ijse.spring.carRental.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+/**
+ * `@authority Tharindu Dilan`
+ * 2:43 PM
+ * 2023-10-22 - 10 - 2023
+ */
+public class UserDTO {
+
+    private String uid;
+    private String userName;
+    private String password;
+    private String type;
+    private String id;
+}
