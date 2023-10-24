f0ce830dacf9e1d292cc7e35e8c24ad1aa9e9513
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-24 14:18:12 +0530

    Update customerData.js

--- FrontEnd/controllers/customerData.js
+++ FrontEnd/controllers/customerData.js
@@ -1,4 +1,4 @@
-//This is where all the customer field and data are verified
+//This is where all the customer field and data are verified and customer json made
 
 
 
@@ -37,45 +37,6 @@ function customerFormData() {
         }
     };
 
-
-
-    /*let formData = new FormData();
-
-    formData.append('cid', '12345');
-    formData.append('name', name);
-    formData.append('email', email);
-    formData.append('address', address);
-    formData.append('contact', tp);
-    formData.append('regDte', "date");
-    formData.append('imageFront', idFront);
-    formData.append('imageBack', idBack);
-    formData.append('dto.uid', '98765'); // Nested UserDTO properties
-    formData.append('dto.userName', regUserName);
-    formData.append('dto.password', pass);
-    formData.append('dto.type', 'customer');
-    formData.append('dto.id', '12345');
-
-    console.log(customerDTO)
-
-    let plainObject = {};
-    for (let [key, value] of formData.entries()) {
-        plainObject[key] = value;
-    }
-    //return JSON.stringify(customerDTO);
-    return formData;*/
-
-    let formData = new FormData();
-
-    for (let key in customerData) {
-        if (customerData[key] instanceof Object && !Array.isArray(customerData[key])) {
-            for (let subKey in customerData[key]) {
-                formData.append(`${key}.${subKey}`, customerData[key][subKey]);
-            }
-        } else {
-            formData.append(key, customerData[key]);
-        }
-    }
-
-    return formData;
+    return JSON.stringify(customerData);
 
 }
