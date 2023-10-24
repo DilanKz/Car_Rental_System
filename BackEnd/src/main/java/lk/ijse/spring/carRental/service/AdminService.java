dd76f2fc70da1715e765bc6b8f56f5787b461547
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-24 21:00:57 +0530

    Create AdminService.java

---
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/service/AdminService.java
@@ -0,0 +1,10 @@
+package lk.ijse.spring.carRental.service;
+
+/**
+ * `@authority Tharindu Dilan`
+ * 8:11 PM
+ * 2023-10-24 - 10 - 2023
+ */
+public interface AdminService {
+    String lastID();
+}
