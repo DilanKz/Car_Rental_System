51493379e5ef53ce04cb8fd15637b9b15744bdc5
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-26 19:28:31 +0530

    Update adminDashboardController.js

--- FrontEnd/controllers/adminDashboardController.js
+++ FrontEnd/controllers/adminDashboardController.js
@@ -4,4 +4,59 @@
     tooltipTriggerList.forEach(tooltipTriggerEl => {
         new bootstrap.Tooltip(tooltipTriggerEl)
     })
-})()
+})();
+
+let dashboardFrame = $('#dashboardFrame');
+let requestsFrame = $('#requestsFrame');
+let carsFrame = $('#carsFrame');
+let accountFrame = $('#accountFrame');
+let profileFrame = $('#profileFrame');
+
+
+let btnDashboard = $('#btnDashboard');
+let btnCars = $('#btnCars');
+let btnAccount = $('#btnAccount');
+let btnRequests = $('#btnRequests');
+
+function hide() {
+    dashboardFrame.css('display','none');
+    requestsFrame.css('display','none');
+    carsFrame.css('display','none');
+    accountFrame.css('display','none');
+    profileFrame.css('display','none');
+
+    btnDashboard.removeClass('bottomLine')
+    btnCars.removeClass('bottomLine')
+    btnAccount.removeClass('bottomLine')
+    btnRequests.removeClass('bottomLine')
+}
+
+hide();
+dashboardFrame.css('display','block');
+btnDashboard.addClass('bottomLine');
+
+$('#btnProfile')
+
+btnDashboard.click(function () {
+    hide();
+    dashboardFrame.css('display','block');
+    $(this).addClass('bottomLine')
+});
+
+btnCars.click(function () {
+    hide();
+    carsFrame.css('display','block');
+    $(this).addClass('bottomLine')
+});
+
+btnRequests.click(function () {
+    hide();
+    requestsFrame.css('display','block');
+    $(this).addClass('bottomLine')
+});
+
+btnAccount.click(function () {
+    hide();
+    accountFrame.css('display','block');
+    $(this).addClass('bottomLine')
+});
