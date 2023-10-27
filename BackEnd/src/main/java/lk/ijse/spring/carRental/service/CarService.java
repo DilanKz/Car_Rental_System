8c319563d817b02de747778ddd53ae1564adaec7
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-27 20:38:43 +0530

    Update CarService.java

--- BackEnd/src/main/java/lk/ijse/spring/carRental/service/CarService.java
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/service/CarService.java
@@ -1,9 +1,14 @@
 package lk.ijse.spring.carRental.service;
 
+import lk.ijse.spring.carRental.dto.CarDTO;
+
 /**
  * `@authority Tharindu Dilan`
  * 4:48 PM
  * 2023-10-27 - 10 - 2023
  */
 public interface CarService {
+    void saveCar(CarDTO dto);
+
+    String lastCarID();
 }
