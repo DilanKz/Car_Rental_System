4bcf1378d249f41bbb48f0c51ecdba9ce6b14567
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-27 16:35:45 +0530

    Update CarRepo.java

--- BackEnd/src/main/java/lk/ijse/spring/carRental/repo/CarRepo.java
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/repo/CarRepo.java
@@ -2,6 +2,7 @@ package lk.ijse.spring.carRental.repo;
 
 import lk.ijse.spring.carRental.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * `@authority Tharindu Dilan`
@@ -9,4 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * 2023-10-27 - 10 - 2023
  */
 public interface CarRepo extends JpaRepository<Car,String> {
+    @Query(value = "SELECT carId FROM car ORDER BY carId DESC LIMIT 1",nativeQuery = true)
+    String getLastID();
 }
