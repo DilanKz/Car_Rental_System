40de0792c55d46fa897a20f6f798e94ef62eebaf
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-27 15:49:57 +0530

    Update UserRegisterController.java

--- BackEnd/src/main/java/lk/ijse/spring/carRental/controller/UserRegisterController.java
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/controller/UserRegisterController.java
@@ -1,17 +1,12 @@
 package lk.ijse.spring.carRental.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lk.ijse.spring.carRental.dto.CustomerDTO;
-import lk.ijse.spring.carRental.dto.responseDTOs.CustomerRespDTO;
 import lk.ijse.spring.carRental.dto.responseDTOs.CustomerResponseDTO;
 import lk.ijse.spring.carRental.dto.responseDTOs.UserResponseDTO;
 import lk.ijse.spring.carRental.service.CustomerService;
 import lk.ijse.spring.carRental.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 /**
  * `@authority Tharindu Dilan`
@@ -28,38 +23,6 @@ public class UserRegisterController {
 
     @Autowired
     ObjectMapper objectMapper;
-
-
-    @PostMapping
-    public ResponseUtil makeAccount(@RequestParam(value = "customerData") String customerDataJson, @RequestPart(value = "images") MultipartFile[] files) throws IOException {
-        System.out.println(customerDataJson);
-
-
-
-        CustomerRespDTO customerData = objectMapper.readValue(customerDataJson, CustomerRespDTO.class);
-        System.out.println(customerData);
-
-        CustomerDTO customerDTO = new CustomerDTO(
-                customerData.getCid(),
-                customerData.getName(),
-                customerData.getEmail(),
-                customerData.getAddress(),
-                customerData.getContact(),
-                customerData.getRegDte(),
-                files[0].getBytes(),
-                files[1].getBytes(),
-                customerData.getDto()
-        );
-
-
-        System.out.println(customerDTO);
-
-        service.saveCustomer(customerDTO);
-        return new ResponseUtil("Ok","Successfully create account",customerDTO);
-    }
-
-
-
 
     @GetMapping("/cusId")
     public ResponseUtil getLastCusId(){
