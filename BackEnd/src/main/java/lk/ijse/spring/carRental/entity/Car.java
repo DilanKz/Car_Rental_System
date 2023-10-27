6b813001032f025f5c634e20384ff84f11e59ae2
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-27 14:55:17 +0530

    create car entity

---
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/entity/Car.java
@@ -0,0 +1,30 @@
+package lk.ijse.spring.carRental.entity;
+
+/**
+ * `@authority Tharindu Dilan`
+ * 2:43 PM
+ * 2023-10-27 - 10 - 2023
+ */
+public class Car {
+    private String carId;
+    private String name;
+    private String regNo;
+    private String color;
+    private String passengers;
+    private String carType;
+    private String carFuelType;
+    private String carState;
+    private String carTransmission;
+    private String waverPay;
+    private String monthlyPayment;
+    private String dailyPayment;
+    private String extraPerKm;
+    private String wholeKm;
+    private String maintained;
+    private String dailyKmLimit;
+    private String monthlyKmLimit;
+    private byte[] carFront;
+    private byte[] carBack;
+    private byte[] carSide;
+    private byte[] carInside;
+}
