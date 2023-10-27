bbb3b7fdaca3a9d3845075aa45e9cad7b502808d
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-27 16:19:04 +0530

    added admin car loader functions

--- FrontEnd/util/frontendUtils.js
+++ FrontEnd/util/frontendUtils.js
@@ -1,4 +1,4 @@
-
+/*=========Customer Register Image Buttons==========*/
 $('#btnIDFront').change(function (event) {
     let file = this.files[0];
     loadImage($('#regIDFrontLoader'),file)
@@ -8,11 +8,30 @@ $('#btnIDBack').change(function (event) {
     let file = this.files[0];
     loadImage($('#regIDBackLoader'),file)
 });
+/*=================Car Add Form====================*/
+
+$('#btnCarFront').change(function (event) {
+    let file = this.files[0];
+    loadImage($('#imgCarFront'),file)
+});
+
+$('#btnCarBack').change(function (event) {
+    let file = this.files[0];
+    loadImage($('#imgCarBack'),file)
+});
+
+$('#btnCarSide').change(function (event) {
+    let file = this.files[0];
+    loadImage($('#imgCarSide'),file)
+});
+
+$('#btnCarIn').change(function (event) {
+    let file = this.files[0];
+    loadImage($('#imgCarIn'),file)
+});
 
 
 function loadImage(image,file) {
-
-
 
     console.log(file)
     const reader = new FileReader();
