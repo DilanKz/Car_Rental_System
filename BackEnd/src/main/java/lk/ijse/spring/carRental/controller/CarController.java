54caed3741cc72889ec1c68f169ee9193e4f2e9e
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-27 15:46:03 +0530

    Create CarController.java

---
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/controller/CarController.java
@@ -0,0 +1,16 @@
+package lk.ijse.spring.carRental.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * `@authority Tharindu Dilan`
+ * 3:45 PM
+ * 2023-10-27 - 10 - 2023
+ */
+@RestController
+@RequestMapping("/customer")
+@CrossOrigin
+public class CarController {
+}
