849b8b19e4238084a33a5da2578b6e4d1ffa3227
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-28 19:40:01 +0530

    refactored util functions

--- FrontEnd/util/frontendUtils.js
+++ FrontEnd/util/frontendUtils.js
@@ -47,3 +47,28 @@ function loadImage(image,file) {
 
 }
 
+function base64ToUint8Array(base64) {
+    const binaryString = window.atob(base64);
+    const length = binaryString.length;
+    const uint8Array = new Uint8Array(length);
+
+    for (let i = 0; i < length; i++) {
+        uint8Array[i] = binaryString.charCodeAt(i);
+    }
+
+    return uint8Array;
+}
+
+
+function byteArrayToImage(byteArray) {
+    const blob = new Blob([base64ToUint8Array(byteArray)], { type: 'image/jpeg' });
+    const imageUrl = URL.createObjectURL(blob);
+
+    return imageUrl;
+}
+
+function byteArrayToFile(byteArray) {
+    const blob = new Blob([base64ToUint8Array(byteArray)], { type: 'image/jpeg' });
+    const file = new File([blob], 'a.jpg', { type: blob.type });
+    return file;
+}
