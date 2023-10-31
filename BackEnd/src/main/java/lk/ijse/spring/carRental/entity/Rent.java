20e7c1191bd95167ed1bf95e69cc9a8f3c8d7410
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-31 16:55:59 +0530

    Update Rent.java

--- BackEnd/src/main/java/lk/ijse/spring/carRental/entity/Rent.java
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/entity/Rent.java
@@ -22,9 +22,15 @@ public class Rent {
     @Id
     private String RentID;
     private String FullPaymentStatus;
+    @Lob
+    private byte[] waiverPaymentSlip;
+    @OneToOne(cascade = {CascadeType.ALL})
+    private Payment payment;
+
     @ManyToOne(cascade = {CascadeType.REFRESH,CascadeType.DETACH})
     @JoinColumn(name = "customerID",referencedColumnName = "cusID",nullable = false)
     private Customer customerID;
     @OneToMany(mappedBy = "rent", cascade = CascadeType.ALL)
     private List<RentDetails> rentDetails;
+
 }
