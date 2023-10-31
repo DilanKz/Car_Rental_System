dbf36e3eff4eb34b08de15ada8b5625933d92149
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-31 22:27:30 +0530

    Create RentServiceImpl.java

---
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/service/impl/RentServiceImpl.java
@@ -0,0 +1,11 @@
+package lk.ijse.spring.carRental.service.impl;
+
+import lk.ijse.spring.carRental.service.RentService;
+
+/**
+ * `@authority Tharindu Dilan`
+ * 10:27 PM
+ * 2023-10-31 - 10 - 2023
+ */
+public class RentServiceImpl implements RentService {
+}
