772adb5306d7264933311cbc9598c1ba5cbfa59d
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-31 17:15:57 +0530

    Create customerPlaceRentController.js

---
+++ FrontEnd/controllers/CustomerControllers/customerPlaceRentController.js
@@ -0,0 +1,40 @@
+
+
+let cartItem=`<div class=" col-11 my-3 rounded-4 box-shadow" style="height: 120px">
+             <div class="p-2 row">
+                 <div class="ms-3 p-0 rounded-4 overflow-hidden" style="height: 100px;width: 100px">
+                     <img src="assets/images/gt3.jpg" alt="" style="height: 100px;width: 100px">
+         
+                 <div class="col-6 ms-4" style="height: 100px">
+                     <div class="row pt-3">
+                         <div class="col-6">
+                             <p style="font-family: sans-serif" class="fw-medium"><span class="fs-5 fw-bold">Waiver :</span> 19000/=</p>
+                             <p style="font-family: sans-serif" class="fw-medium"><span class="fs-5 fw-bold">Price  :</span> 19000/=</p>
+         
+                         <div class="col-6">
+                             <p style="font-family: sans-serif" class="fw-medium"><span class="fs-5 fw-bold">Waiver :</span> 19000/=</p>
+                             <p style="font-family: sans-serif" class="fw-medium"><span class="fs-5 fw-bold">Price  :</span> 19000/=</p>
+                         </div>
+                     </div>
+                 </div>
+                 <div class="col-2 ms-2 d-flex justify-content-center align-items-center" style="height: 100px">
+                     <div class="form-check">
+                         <input class="form-check-input text-success checked"
+                                type="checkbox"
+                                value=""
+                                id="flexCheckDefault"/>
+                         <label class="form-check-label"
+                                for="flexCheckDefault">Driver</label>
+                     </div>
+         
+                 <div class="col-1 ms-2 d-flex justify-content-center align-items-center" style="height: 100px">
+                     <button type="button" class="btnItemClose btn-close p-2 box-shadow-2 bg-light"></button>
+                 </div>
+             </div>
+         </div>`
+
+let selectedCarList
+
+function addCarToTheCart(id,list) {
+    let listElement = list[id];
+}
