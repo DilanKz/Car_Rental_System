577fbad8193a0e34130a30f0f4ab3a99dc4e6ad0
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-31 17:27:11 +0530

    Update customerPlaceRentController.js

--- FrontEnd/controllers/CustomerControllers/customerPlaceRentController.js
+++ FrontEnd/controllers/CustomerControllers/customerPlaceRentController.js
@@ -1,19 +1,26 @@
+let selectedCarList=[]
+
+function addCarToTheCart(index,list) {
+    let listElement = list[index];
+    selectedCarList.push(listElement);
+
+    let image = byteArrayToImage(listElement.carFront);
 
 
-let cartItem=`<div class=" col-11 my-3 rounded-4 box-shadow" style="height: 120px">
+    let cartItem=`<div class=" col-11 my-3 rounded-4 box-shadow" style="height: 120px">
              <div class="p-2 row">
                  <div class="ms-3 p-0 rounded-4 overflow-hidden" style="height: 100px;width: 100px">
-                     <img src="assets/images/gt3.jpg" alt="" style="height: 100px;width: 100px">
+                     <img src="${image}" alt="" style="height: 100px;width: 100px">
          
                  <div class="col-6 ms-4" style="height: 100px">
                      <div class="row pt-3">
                          <div class="col-6">
-                             <p style="font-family: sans-serif" class="fw-medium"><span class="fs-5 fw-bold">Waiver :</span> 19000/=</p>
-                             <p style="font-family: sans-serif" class="fw-medium"><span class="fs-5 fw-bold">Price  :</span> 19000/=</p>
+                             <p style="font-family: sans-serif" class="fw-medium"><span class="fs-5 fw-bold">Waiver :</span> ${listElement.waiverPay}/=</p>
+                             <p style="font-family: sans-serif" class="fw-medium"><span class="fs-5 fw-bold">Daily  :</span> ${listElement.dailyPayment}/=</p>
          
                          <div class="col-6">
-                             <p style="font-family: sans-serif" class="fw-medium"><span class="fs-5 fw-bold">Waiver :</span> 19000/=</p>
-                             <p style="font-family: sans-serif" class="fw-medium"><span class="fs-5 fw-bold">Price  :</span> 19000/=</p>
+                             <p style="font-family: sans-serif" class="fw-medium"><span class="fs-5 fw-bold">Monthly :</span> ${listElement.monthlyPayment}/=</p>
+                             <p style="font-family: sans-serif" class="fw-medium"><span class="fs-5 fw-bold">Extra  :</span> ${listElement.extraPerKm}/=</p>
                          </div>
                      </div>
                  </div>
@@ -33,8 +40,4 @@ let cartItem=`<div class=" col-11 my-3 rounded-4 box-shadow" style="height: 120p
              </div>
          </div>`
 
-let selectedCarList
-
-function addCarToTheCart(id,list) {
-    let listElement = list[id];
 }
