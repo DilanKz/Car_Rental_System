fd6bd9ec14474c7f2f12a1f2fba42456e1bc32eb
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-31 21:50:56 +0530

    Create PlaceRequestController.java

---
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/controller/PlaceRequestController.java
@@ -0,0 +1,23 @@
+package lk.ijse.spring.carRental.controller;
+
+import lk.ijse.spring.carRental.dto.RentDTO;
+import lk.ijse.spring.carRental.util.ResponseUtil;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * `@authority Tharindu Dilan`
+ * 9:14 PM
+ * 2023-10-31 - 10 - 2023
+ */
+@RestController
+@RequestMapping("/Request")
+@CrossOrigin
+public class PlaceRequestController {
+
+    @PostMapping
+    public ResponseUtil placeRequest(@RequestBody RentDTO dto){
+        System.out.println(dto);
+        return new ResponseUtil("","",dto);
+    }
+}
