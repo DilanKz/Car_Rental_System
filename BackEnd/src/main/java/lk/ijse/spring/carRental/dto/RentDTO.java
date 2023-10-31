f9a9d6b420f3c5d0b3d23ace9728804ccb25b343
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-31 21:20:18 +0530

    Update RentDTO.java

--- BackEnd/src/main/java/lk/ijse/spring/carRental/dto/RentDTO.java
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/dto/RentDTO.java
@@ -1,10 +1,13 @@
 package lk.ijse.spring.carRental.dto;
 
 import lk.ijse.spring.carRental.entity.Payment;
+import lk.ijse.spring.carRental.entity.RentDetails;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+
+import java.util.List;
 
 /**
  * `@authority Tharindu Dilan`
@@ -25,4 +28,5 @@ public class RentDTO {
     private String state;
     private byte[] waiverPaymentSlip;
     private Payment payment;
+    private List<RentDetails> rentDetails;
 }
