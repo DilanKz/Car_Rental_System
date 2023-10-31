4cd6f69065749a62d40b13d4431883e9279bfc8f
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-10-31 22:30:17 +0530

    Update RentServiceImpl.java

--- BackEnd/src/main/java/lk/ijse/spring/carRental/service/impl/RentServiceImpl.java
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/service/impl/RentServiceImpl.java
@@ -3,12 +3,17 @@ package lk.ijse.spring.carRental.service.impl;
 import lk.ijse.spring.carRental.repo.RentRepo;
 import lk.ijse.spring.carRental.service.RentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 /**
  * `@authority Tharindu Dilan`
  * 10:27 PM
  * 2023-10-31 - 10 - 2023
  */
+@Service
+@Transactional
 public class RentServiceImpl implements RentService {
     @Autowired
     RentRepo rentRepo;
