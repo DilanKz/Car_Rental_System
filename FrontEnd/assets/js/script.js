371396087de525b554008e3f7222a603d9c4a7ff
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-11-01 10:33:39 +0530

    Update script.js

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,7 +1,20 @@
 let btnRent = $('#rentIt');
 
 btnRent.click(function () {
-    let liveToast = $('#loginToast');
-    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(liveToast);
+    blueToastShow('Please sign in to Place a rent');
+    yellowToastShow('Please sign in to Place a rent');
+});
+
+function blueToastShow(message) {
+    $('#toastBlueText').text(message);
+    const toastBLueFrame = $('#toastBLueFrame');
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastBLueFrame);
     toastBootstrap.show();
-})
+}
+
+function yellowToastShow(message) {
+    $('#toastYellowText').text(message);
+    const toastBLueFrame = $('#toastYellowFrame');
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastBLueFrame);
+    toastBootstrap.show();
+}
