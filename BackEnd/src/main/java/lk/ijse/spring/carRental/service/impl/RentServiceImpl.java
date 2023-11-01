02385e07a1fbc05d9d87f9177cc962348ee2420d
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-11-01 11:05:55 +0530

    Update RentServiceImpl.java

--- BackEnd/src/main/java/lk/ijse/spring/carRental/service/impl/RentServiceImpl.java
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/service/impl/RentServiceImpl.java
@@ -1,11 +1,13 @@
 package lk.ijse.spring.carRental.service.impl;
 
+import lk.ijse.spring.carRental.repo.DriverRepo;
 import lk.ijse.spring.carRental.repo.RentRepo;
 import lk.ijse.spring.carRental.service.RentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 /**
  * `@authority Tharindu Dilan`
@@ -17,9 +19,16 @@ import javax.transaction.Transactional;
 public class RentServiceImpl implements RentService {
     @Autowired
     RentRepo rentRepo;
+    @Autowired
+    DriverRepo driverRepo;
 
     @Override
     public String lastID(){
         return rentRepo.getLastID();
+    }
+
+    @Override
+    public List<String> getAvailableDrivers(){
+        return driverRepo.getAllDID();
     }
 }
