71283a6469cc121f7a8852d09436922f0fd8d798
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-11-02 19:25:30 +0530

    added get all function

--- FrontEnd/controllers/adminControllers/adminRequestController.js
+++ FrontEnd/controllers/adminControllers/adminRequestController.js
@@ -1,19 +1,38 @@
-$('#tblRequest');
+let tblRequest = $('#tblRequest');
+
+
 
 function loadAllRequests() {
     $.ajax({
-        url: 'http://localhost:8080/requst/',
+        url: 'http://localhost:8080/CarRental/Request/allRents',
         method: 'GET',
         success: function (res) {
-            containerView.empty();
-            carsList = res.data;
-            for (let i = 0; i < carsList.length; i++) {
-                addCarToView(carsList[i]);
-                $('#carLoader').css('display', 'none')
+            console.log(res.data);
+            tblRequest.empty();
+            let dataList = res.data;
+
+            for (let i = 0; i < dataList.length; i++) {
+
+                addTable(dataList[i])
+
             }
+
         },
         error: function (error) {
             console.error('Error:', error);
         }
     });
+}
+
+function addTable(rent) {
+    let tr = `<tr>
+                  <th>${rent.customerID}</th>
+                  <th>${rent.rentID}</th>
+                  <th>${rent.addDate}</th>
+                  <th>${rent.fullPaymentStatus}</th>
+                  <th>${rent.pickupDate}</th>
+                  <th>${rent.estReturnDate}</th>
+              </tr>`
+
+    tblRequest.append(tr);
 }
