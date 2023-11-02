ea1d15c48c93979e442e748d6c3edc66fe71f59c
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-11-02 22:34:33 +0530

    Update RentRepo.java

--- BackEnd/src/main/java/lk/ijse/spring/carRental/repo/RentRepo.java
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/repo/RentRepo.java
@@ -2,7 +2,9 @@ package lk.ijse.spring.carRental.repo;
 
 import lk.ijse.spring.carRental.entity.Rent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * `@authority Tharindu Dilan`
@@ -12,4 +14,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface RentRepo extends JpaRepository<Rent,String> {
     @Query(value = "SELECT RentID FROM rent ORDER BY RentID DESC LIMIT 1",nativeQuery = true)
     String getLastID();
+    @Modifying
+    @Query(value = "UPDATE Rent SET state ='approved' WHERE RentID=:id")
+    void updateStatus(@Param("id") String id);
 }
