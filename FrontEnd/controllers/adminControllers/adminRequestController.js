0ebede02015e4ad3dcf9fbde56aef0452b01285b
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-11-02 18:56:02 +0530

    Update adminRequestController.js

--- FrontEnd/controllers/adminControllers/adminRequestController.js
+++ FrontEnd/controllers/adminControllers/adminRequestController.js
@@ -1,5 +1,19 @@
 $('#tblRequest');
 
 function loadAllRequests() {
-
+    $.ajax({
+        url: 'http://localhost:8080/requst/',
+        method: 'GET',
+        success: function (res) {
+            containerView.empty();
+            carsList = res.data;
+            for (let i = 0; i < carsList.length; i++) {
+                addCarToView(carsList[i]);
+                $('#carLoader').css('display', 'none')
+            }
+        },
+        error: function (error) {
+            console.error('Error:', error);
+        }
+    });
 }
