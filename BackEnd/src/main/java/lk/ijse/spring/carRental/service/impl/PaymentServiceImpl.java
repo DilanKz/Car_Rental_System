81b370776495ebf90c0c213cc03b5d639aaaac5f
DilanKz <Dilanmallawarachchi555@gmail.com>
2023-11-03 19:55:49 +0530

    Update PaymentServiceImpl.java

--- BackEnd/src/main/java/lk/ijse/spring/carRental/service/impl/PaymentServiceImpl.java
+++ BackEnd/src/main/java/lk/ijse/spring/carRental/service/impl/PaymentServiceImpl.java
@@ -1,11 +1,15 @@
 package lk.ijse.spring.carRental.service.impl;
 
+import lk.ijse.spring.carRental.dto.PaymentDTO;
 import lk.ijse.spring.carRental.repo.PaymentRepo;
 import lk.ijse.spring.carRental.service.PaymentService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 /**
  * `@authority Tharindu Dilan`
@@ -18,8 +22,16 @@ public class PaymentServiceImpl implements PaymentService {
     @Autowired
     PaymentRepo repo;
 
+    @Autowired
+    ModelMapper mapper;
+
     @Override
     public String lastID(){
         return repo.getLastID();
+    }
+
+    @Override
+    public List<PaymentDTO> loadAllPayments(){
+        return mapper.map(repo.findAll(),new TypeToken<PaymentDTO>(){}.getType());
     }
 }
